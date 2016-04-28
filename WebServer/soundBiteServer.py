1944c8da7ce81cbaf7ba215fad403bfefe8e78fd
Christopher Lambert <lamberce@github.com>
2016-04-28 14:07:11 -0400

    Upload working, Login working, Create Account working

--- WebServer/soundBiteServer.py
+++ WebServer/soundBiteServer.py
@@ -1,31 +1,117 @@
 import os, os.path
+import hashlib
 import random
 import string
 import cherrypy
 import sys
+import gridfs
 from cherrypy.lib import sessions
+import riak
 from riak import RiakClient, RiakNode
+from pymongo import MongoClient
 from jinja2 import Environment, FileSystemLoader
 
 # declare global variables
 env = Environment(loader=FileSystemLoader('staticFiles/templates'))
 riakClient = RiakClient(protocol='http', host='datatwo.csse.rose-hulman.edu', http_port=8098)
 userbucketName = "usernames"
+mongoClient = MongoClient('dataone.csse.rose-hulman.edu', 27017)
+mongoDB = mongoClient.test
 
 class ServeSite(object):
 	@cherrypy.expose
-	def index(self):
-		tmpl = env.get_template('index.html')
-		return tmpl.render()
+	def index(self, message=''):
+		if(cherrypy.session.get('loggedIn', 'None') == True):
+			raise cherrypy.HTTPRedirect("/uploadPage")
+		else:
+			tmpl = env.get_template('home.html')
+			return tmpl.render()%message
+
+	@cherrypy.expose
+	def register(self,errorMessage=""):
+		if(cherrypy.session.get('loggedIn', 'None') == True):
+			raise cherrypy.HTTPRedirect("/uploadPage")
+		else:
+			tmpl = env.get_template('register.html')
+			return tmpl.render()%errorMessage
 
 	@cherrypy.expose
 	def createUser(self, name, username, password):
 		userBucket = riakClient.bucket(userbucketName)
-		userValues = {"name": name, "password": password, "salt": username + "salt"}
-		userKey = userBucket.new(username, data=userValues)
-		userKey.store()
-		tmpl = env.get_template('index.html')
-		return tmpl.render()
+		if(username in userBucket.get_keys()):
+			raise cherrypy.HTTPRedirect("/register?errorMessage='Username already Taken'")
+		else:
+			salt = username+"salt"
+			password = hashlib.sha1(password+salt).hexdigest()
+			userValues = {"name": name, "password": password, "salt": username + "salt"}
+			userKey = userBucket.new(username, data=userValues)
+			userKey.store()
+			cherrypy.session['loggedIn'] = True
+			cherrypy.session['username'] = username
+			raise cherrypy.HTTPRedirect("/uploadPage")
+
+	# for testing
+	@cherrypy.expose
+	def displayUsers(self):
+		userBucket = riakClient.bucket(userbucketName)
+		users = ""
+		for user in userBucket.get_keys():
+			users = users + user + '\n'
+		return users
+
+	@cherrypy.expose
+	def login(self, username, password):
+		userBucket = riakClient.bucket(userbucketName)
+		if(username in userBucket.get_keys()):
+			userData = userBucket.get(username).data
+			associatedPassword = userData['password']
+			salt = userData['salt']
+			hashedPassword = hashlib.sha1(password+salt).hexdigest()
+			if(associatedPassword == hashedPassword):
+				cherrypy.session['loggedIn'] = True
+				cherrypy.session['username'] = username
+				raise cherrypy.HTTPRedirect("/uploadPage")
+			else:
+				cherrypy.session['loggedIn'] = False
+				raise cherrypy.HTTPRedirect("/index?message='invalid password'")
+		else:
+			raise cherrypy.HTTPRedirect("/index?message='user does not exist'")
+	
+	@cherrypy.expose
+	def signOut(self):
+		cherrypy.session['loggedIn'] = False
+		cherrypy.session['username'] = None
+		raise cherrypy.HTTPRedirect("/index")
+	
+	@cherrypy.expose
+	def uploadPage(self, uploadMessage=''):
+		if(cherrypy.session.get('loggedIn', 'None')==True):
+			tmpl = env.get_template('uploadPage.html')
+			return tmpl.render()%uploadMessage
+		else:
+			raise cherrypy.HTTPRedirect("/index")
+
+	@cherrypy.expose
+	def uploadMP3(self, uploadedFile, sName):
+		if(uploadedFile.file is None):
+			return """Must provide a file"""
+		elif(cherrypy.session.get('loggedIn', 'None') == False):
+			raise cherrypy.HTTPRedirect("/uploadPage?uploadMessage='Must be logged in!'")
+		else:			
+			fileData = uploadedFile.file.read()
+			fs = gridfs.GridFS(mongoDB)
+			uName = cherrypy.session.get('username', 'None')
+			filePutID = fs.put(fileData, filename=sName, username=uName)
+			# for testing to ensure file was uploaded
+	
+	# for testing
+	@cherrypy.expose
+	def uploadedSounds(self):
+		return "Going to display all uploaded file names here"
+			
+	@cherrypy.expose
+	def allFiles(self):
+		return """Going to display all files here for testing purposes"""
 
 if __name__ == '__main__':
 	conf = {
@@ -42,7 +128,9 @@ if __name__ == '__main__':
 		}
 	}
 
+
 cherrypy.server.socket_host = '127.0.0.1'
+cherrypy.server.socket_port = 8080
 cherrypy.quickstart(ServeSite(), '/',conf)
 
 
