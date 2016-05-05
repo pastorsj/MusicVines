e954551e68d9a6a83956c7302fd4415b271d8968
Joel Shapiro <joel.shapiro7@gmail.com>
2016-05-05 18:57:06 -0400

    removed extra data

--- Neo4jStuff/Neo4jTesting.py
+++ Neo4jStuff/Neo4jTesting.py
@@ -6,9 +6,9 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 graph_db = GraphDatabase.driver("bolt://datathree.csse.rose-hulman.edu", auth=basic_auth("neo4j", os.environ["neo4jpass"]))
 
-def neoAddSong(songID, title, uploader, desc):
+def neoAddSong(songID, title):
     session = graph_db.session()
-    session.run("CREATE (node:Song {ID : " + str(songID) + ", Title : \'" +title+ "\', Uploader: \'"+uploader+"\', Description : \'"+desc+"\'})")
+    session.run("CREATE (node:Song {ID : " + str(songID) + ", Title : \'" +title+ "\'})")
     return
 
 def neoAddTag(songID, tag):
@@ -26,11 +26,11 @@ def neoDeleteTag(songID, tag):
                 "Delete f")
     return
 
-def neoReturnID(songTitle, uploader):
-    session = graph_db.session()
-    return session.run("MATCH (a:Song) where a.Title = \'" + songTitle + "\' "
-                       "and a.Uploader = \'" + uploader + "\' " +
-                       "return a.ID")
+# def neoReturnID(songTitle, uploader):
+#     session = graph_db.session()
+#     return session.run("MATCH (a:Song) where a.Title = \'" + songTitle + "\' "
+#                        "and a.Uploader = \'" + uploader + "\' " +
+#                        "return a.ID")
 
 def neoGetSimilar(songID):
     session = graph_db.session()
